6c34bb8a006994ef2f16d763c32cf895885cda30
chtangchtang <1587758421@qq.com>
2023-11-19 02:24:25 +0800

    latest

--- test/PBFT/gen_network.py
+++ test/PBFT/gen_network.py
@@ -14,13 +14,13 @@ new_num = int(sys.argv[1])
 old_num = int(list(content['services'].keys())[-1][-1]) + 1
 for i in range(old_num, new_num):
     # abac-tp
-    tmp = copy.copy(content['services']['abac-tp-python-0'])
+    tmp = copy.deepcopy(content['services']['abac-tp-python-0'])
     tmp['container_name'] = 'abac-tp-python-default-' + str(i)
     tmp['hostname'] = 'abac-tp-python-default-' + str(i)
     tmp['entrypoint'] = tmp['entrypoint'].replace('validator-0', 'validator-' + str(i))
     content['services']['abac-tp-python-' + str(i)] = tmp
     # validator
-    tmp = copy.copy(content['services']['validator-' + str(i-1)])
+    tmp = copy.deepcopy(content['services']['validator-' + str(i-1)])
     tmp['container_name'] = 'sawtooth-validator-default-' + str(i)
     tmp['hostname'] = 'sawtooth-validator-default-' + str(i)
     tmp['command'] = tmp['command'].replace('validator-' + str(i-1), 'validator-' + str(i))
@@ -29,21 +29,22 @@ for i in range(old_num, new_num):
     tmp['command'] = ''.join(tmp['command'])
     content['services']['validator-' + str(i)] = tmp
     # rest-api
-    tmp = copy.copy(content['services']['rest-api-0'])
+    tmp = copy.deepcopy(content['services']['rest-api-0'])
     tmp['container_name'] = 'rest-api-' + str(i)
     tmp['hostname'] = 'rest-api-' + str(i)
+    tmp['ports'][0] = '800' + str(i) + ':8008'
     tmp['depends_on'][0] = tmp['depends_on'][0].replace('0', str(i))
     tmp['command'] = tmp['command'].replace('validator-0', 'validator-' + str(i))
     tmp['command'] = tmp['command'].replace('rest-api-0', 'rest-api-' + str(i))
     content['services']['rest-api-' + str(i)] = tmp
     # settings-tp
-    tmp = copy.copy(content['services']['settings-tp-0'])
+    tmp = copy.deepcopy(content['services']['settings-tp-0'])
     tmp['container_name'] = 'settings-tp-' + str(i)
     tmp['hostname'] = 'settings-tp-' + str(i)
     tmp['entrypoint'] = tmp['entrypoint'].replace('validator-0', 'validator-' + str(i))
     content['services']['settings-tp-' + str(i)] = tmp
     # pbft-engine
-    tmp = copy.copy(content['services']['pbft-0'])
+    tmp = copy.deepcopy(content['services']['pbft-0'])
     tmp['container_name'] = 'sawtooth-pbft-engine-default-' + str(i)
     tmp['hostname'] = 'sawtooth-pbft-engine-default-' + str(i)
     tmp['entrypoint'] = tmp['entrypoint'].replace('validator-0', 'validator-' + str(i))
