649ce7f5c3032eb7668c69def3b0974496cf0152
chtangchtang <1587758421@qq.com>
2023-11-19 01:30:06 +0800

    latest

---
+++ test/PBFT/gen_network.py
@@ -0,0 +1,54 @@
+import yaml
+import sys
+import copy
+
+
+# Read basic compose file
+f = open('5nodes.yaml', 'r')
+content = yaml.load(f.read(), Loader=yaml.FullLoader)
+f.close()
+
+# Add services
+new_num = int(sys.argv[1]) - 5
+old_num = int(list(content['services'].keys())[-1][-1]) + 1
+for i in range(old_num, new_num):
+    # abac-tp
+    tmp = copy.copy(content['services']['abac-tp-python-0'])
+    tmp['container_name'] = 'abac-tp-python-default-' + str(i)
+    tmp['hostname'] = 'abac-tp-python-default-' + str(i)
+    tmp['entrypoint'] = tmp['entrypoint'].replace('validator-0', 'validator-' + str(i))
+    content['services']['abac-tp-python-' + str(i)] = tmp
+    # validator
+    tmp = copy.copy(content['services']['validator-' + str(i-1)])
+    tmp['container_name'] = 'sawtooth-validator-default-' + str(i)
+    tmp['hostname'] = 'sawtooth-validator-default-' + str(i)
+    tmp['command'] = tmp['command'].replace('validator-' + str(i-1), 'validator-' + str(i))
+    tmp['command'] = list(tmp['command'])
+    tmp['command'].insert(-3, ' \\\n    --peers tcp://validator-' + str(i-1) + ':8800')
+    tmp['command'] = ''.join(tmp['command'])
+    content['services']['validator-' + str(i)] = tmp
+    # rest-api
+    tmp = copy.copy(content['services']['rest-api-0'])
+    tmp['container_name'] = 'rest-api-' + str(i)
+    tmp['hostname'] = 'rest-api-' + str(i)
+    tmp['depends_on'][0] = tmp['depends_on'][0].replace('0', str(i))
+    tmp['command'] = tmp['command'].replace('validator-0', 'validator-' + str(i))
+    tmp['command'] = tmp['command'].replace('rest-api-0', 'rest-api-' + str(i))
+    content['services']['rest-api-' + str(i)] = tmp
+    # settings-tp
+    tmp = copy.copy(content['services']['settings-tp-0'])
+    tmp['container_name'] = 'settings-tp-' + str(i)
+    tmp['hostname'] = 'settings-tp-' + str(i)
+    tmp['entrypoint'] = tmp['entrypoint'].replace('validator-0', 'validator-' + str(i))
+    content['services']['settings-tp-' + str(i)] = tmp
+    # pbft-engine
+    tmp = copy.copy(content['services']['pbft-0'])
+    tmp['container_name'] = 'sawtooth-pbft-engine-default-' + str(i)
+    tmp['hostname'] = 'sawtooth-pbft-engine-default-' + str(i)
+    tmp['entrypoint'] = tmp['entrypoint'].replace('validator-0', 'validator-' + str(i))
+    content['services']['pbft-' + str(i)] = tmp
+
+# Write new compose file
+f = open(sys.argv[1] + 'nodes.yaml', 'w')
+yaml.dump(content, f, Dumper=yaml.SafeDumper)
+f.close()
