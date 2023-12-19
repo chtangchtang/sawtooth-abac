6088149b9f09d2229bffbcfbf1a0f5abbeaeb76f
chtangchtang <1587758421@qq.com>
2023-12-20 02:55:48 +0800

    latest

--- test/add_policy/test_add_policy_10.py
+++
@@ -1,7 +0,0 @@
-import time
-import os
-
-
-for i in range(10000):
-    os.system("abac add data/policy" + str(i) + ".json --url 172.18.0.27:8008")
-    time.sleep(0.1)
---
+++ test/test_check_inquiry.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import time
+
+DEFAULT_TXS = 100
+
+send_rate = int(sys.argv[1])
+url = sys.argv[2]
+
+for i in range(DEFAULT_TXS):
+    os.system("abac check example/inquiry.json --url " + url)
+    time.sleep(1 / send_rate)
