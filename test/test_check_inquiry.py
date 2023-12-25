95c2e8d41cde3525ec13041fb2b2cb366a215019
chtangchtang <1587758421@qq.com>
2023-12-26 04:07:36 +0800

    latest

--- test/test_check_inquiry.py
+++ test/test_check_inquiry.py
@@ -9,11 +9,13 @@ DEFAULT_TXS = 100
 send_rate = int(sys.argv[1])
 url = sys.argv[2]
 
-# write start epoch time.
+# write start epoch time
 client = InfluxDBClient(host='172.21.105.144', port='8086', username='admin', password='admin', database='metrics')
 points = []
 points.append({"measurement": "start_test_check_inquiry", "fields": {'epoch_time': time.time_ns()}})
+client.write_points(points)
 
+# start test
 for i in range(DEFAULT_TXS):
     os.system("abac check inquiry.json --url " + url)
     time.sleep(1 / send_rate)
--- test/test_delete_policy.py
+++ test/test_delete_policy.py
@@ -9,12 +9,13 @@ DEFAULT_TXS = 100
 send_rate = int(sys.argv[1])
 url = sys.argv[2]
 
-# write start epoch time.
+# write start epoch time
 client = InfluxDBClient(host='172.21.105.144', port='8086', username='admin', password='admin', database='metrics')
 points = []
 points.append({"measurement": "start_test_delete_policy", "fields": {'epoch_time': time.time_ns()}})
-
 client.write_points(points)
+
+# start test
 for i in range(DEFAULT_TXS):
     os.system("abac delete data/policy" + str(i) + ".json --url " + url)
     time.sleep(1 / send_rate)
