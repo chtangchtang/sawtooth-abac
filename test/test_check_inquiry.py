ab6aa02de02547a03fec44edcfa7de8e1d258313
chtangchtang <1587758421@qq.com>
2024-03-06 00:03:36 +0800

    latest

--- test/test_check_inquiry.py
+++ test/test_check_inquiry.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import socket
 from influxdb import InfluxDBClient
 
 
@@ -10,12 +11,12 @@ send_rate = int(sys.argv[1])
 url = sys.argv[2]
 
 # write start epoch time
-client = InfluxDBClient(host='172.21.105.144', port='8086', username='admin', password='admin', database='metrics')
+client = InfluxDBClient(host=socket.gethostbyname(socket.gethostname()), port='8086', username='admin', password='admin', database='metrics')
 points = []
 points.append({"measurement": "start_test_check_inquiry", "fields": {'epoch_time': time.time()}})
 client.write_points(points)
 
 # start test
 for i in range(DEFAULT_TXS):
-    os.system("abac check data/inquiry.json --url " + url)
+    os.system("abac check data/inquiry.json --url " + url + " &")
     time.sleep(1 / send_rate)
