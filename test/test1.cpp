dc98f393816c371a1228867439bde23e7e9c0c8c
chtangchtang <1587758421@qq.com>
2024-04-07 00:13:48 +0800

    latesy

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,51 +1,37 @@
 #include <iostream>
-#include <fstream>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <chrono>
-using namespace std;
+#include <fstream>
+#include <jsoncpp/json/json.h>
 
-void readFromFile(const string& filename) {
-    ifstream file(filename);
-    if (file) {
-        // 读取文件中的数据
-        string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+void readJsonFile(const char* filename) {
+    // 读取JSON文件
+    std::ifstream file(filename);
+    Json::Value jsonData;
+    file >> jsonData;
 
-        // 在控制台打印读取的数据
-        // cout << "Child process " << getpid() << " read: " << content << endl;
-
-        // 关闭文件
-        file.close();
-    } else {
-        cerr << "Error opening file " << filename << endl;
-    }
+    // 在这里处理jsonData，例如输出到控制台
+    std::cout << jsonData << std::endl;
 }
 
 int main() {
-    string filename = "data/inquiry.json";
+    const char* filename = "data/inquiry.json";
+    const int numProcesses = 1000;
 
-    int numProcesses = 1000;
-    auto epoch_time = chrono::system_clock::now().time_since_epoch();
-    long long epoch_seconds = chrono::duration_cast<chrono::seconds>(epoch_time).count();
-    // 创建一千个子进程来读取文件的全部内容
     for (int i = 0; i < numProcesses; ++i) {
         pid_t pid = fork();
-        if (pid == 0) {
-            // 子进程读取文件的全部内容
-            readFromFile(filename);
-            return 0;
+
+        if (pid == 0) { // 子进程
+            readJsonFile(filename);
+            exit(0); // 子进程完成后退出
         } else if (pid < 0) {
-            cerr << "Error forking process" << endl;
-            return 1;
+            std::cerr << "Failed to fork process" << std::endl;
+            exit(1);
         }
     }
 
-    // 等待所有子进程结束
-    for (int i = 0; i < numProcesses; ++i) {
-        wait(NULL);
-    }
-    auto epoch_time1 = chrono::system_clock::now().time_since_epoch();
-    long long epoch_seconds1 = chrono::duration_cast<chrono::seconds>(epoch_time1).count();
-    cout << "Time elapsed: " << epoch_seconds1 - epoch_seconds << " seconds" << endl;
+    // 等待所有子进程完成
+    while (wait(nullptr) > 0) {}
+
     return 0;
 }
