9c22d931f55f898d64c5e7afd18132e79ed90161
chtangchtang <1587758421@qq.com>
2024-05-26 15:24:11 +0800

    latest

--- analysis/calculate_time1.py
+++ analysis/calculate_time1.py
@@ -46,7 +46,7 @@ for row in csv_reader:
                          flags[4] = True
         if flags[0] and flags[1] and flags[2] and flags[3] and flags[4]:
            end_time = max(times)
-           print('end_time:', end_time, end=',')
+        #    print('end_time:', end_time, end=',')
         #    time_usage = (int(end_time) - int(start_time)) / 1000000000
         #    print('time_usage:', time_usage, end=',')
         #    print('throughput:', 1000 / time_usage)
