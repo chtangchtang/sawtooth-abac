e18a93d67991d3c557cf8e423218fe299e6b7770
chtangchtang <1587758421@qq.com>
2024-05-26 16:57:07 +0800

    latest

--- analysis/calculate_time1.py
+++ analysis/calculate_time1.py
@@ -6,40 +6,36 @@ filename = sys.argv[1]
 flags = [False, False, False, False, False]
 times = []
 csv_reader = csv.reader(open(filename))
-count = 1001
-condition = 'count=' + str(count)
-# start_time = ''
 for row in csv_reader:
     try:
         # if row[0].find('start') > -1:
         #     data = row[0].split(' ')
         #     start_time = data[2]
         #     print('start_time:', start_time, end=',')
-        #     flags = [False, False, False, False, False]
         if row[0] == 'sawtooth_validator.chain.ChainController.committed_transactions_count':
             data = row[1].split(' ')
             if data[0] == 'host=sawtooth-validator-default-0':
-                if data[1] == condition:
+                if data[1] == 'count=1001':
                     if not flags[0]:
                         times.append(data[2])
                         flags[0] = True
             if data[0] == 'host=sawtooth-validator-default-1':
-                if data[1] == condition:
+                if data[1] == 'count=1001':
                     if not flags[1]:
                         times.append(data[2])
                         flags[1] = True
             if data[0] == 'host=sawtooth-validator-default-2':
-                if data[1] == condition:
+                if data[1] == 'count=1001':
                     if not flags[2]:
                         times.append(data[2])
                         flags[2] = True
             if data[0] == 'host=sawtooth-validator-default-3':
-                if data[1] == condition:
+                if data[1] == 'count=1001':
                     if not flags[3]:
                         times.append(data[2])
                         flags[3] = True
             if data[0] == 'host=sawtooth-validator-default-4':
-                 if data[1] == condition:
+                 if data[1] == 'count=1001':
                      if not flags[4]:
                          times.append(data[2])
                          flags[4] = True
@@ -49,8 +45,6 @@ for row in csv_reader:
         #    time_usage = (int(end_time) - int(start_time)) / 1000000000
         #    print('time_usage:', time_usage, end=',')
         #    print('throughput:', 1000 / time_usage)
-           count += 1000
-           condition = 'count=' + str(count)
            flags = [False, False, False, False, False]
     finally:
         pass
