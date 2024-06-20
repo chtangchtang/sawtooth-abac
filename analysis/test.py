3fe5583db99c911f93ea9efd70efc28db04a6a1e
chtangchtang <1587758421@qq.com>
2024-06-21 02:50:53 +0800

    latest

--- analysis/test.py
+++ analysis/test.py
@@ -1,18 +1,4 @@
-import csv
-import sys
-
-
-filename = sys.argv[1]
-flags = [False, False, False, False, False]
-times = []
-csv_reader = csv.reader(open(filename))
-count = 1001
-condition = 'count=' + str(count)
-for row in csv_reader:
-    try:
-        if row[0].find('start') > -1:
-            data = row[0].split(' ')
-            start_time = data[2]
-            print(start_time, end=',')
-    finally:
-        pass
+print((1716715618000000000-1716715476582944512) / 1000000000)
+print((1716715849000000000-1716715702223066846) / 1000000000)
+print(1000 / ((1716715618000000000-1716715476582944512) / 1000000000))
+print(1000 / ((1716715849000000000-1716715702223066846) / 1000000000))
