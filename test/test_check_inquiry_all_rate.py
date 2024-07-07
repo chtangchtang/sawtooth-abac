daa32bfcbaabb2d103e313349b084eefb9c25f7c
chtangchtang <1587758421@qq.com>
2024-07-07 20:54:50 +0800

    latest

---
+++ test/test_check_inquiry_all_rate.py
@@ -0,0 +1,11 @@
+import sys
+import subprocess
+import time
+
+
+ip = sys.argv[1]
+
+for i in range(10):
+    for i in range(5):
+        subprocess.run('python3 test_check_inquiry.py '+ str(i * 2 + 3) + ' ' + ip + ':8008', shell=True)
+        time.sleep(30)
