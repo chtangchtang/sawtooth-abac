da8ec3f37f9eef92bc38b4d207257c190e370296
chtangchtang <1587758421@qq.com>
2024-07-10 15:08:54 +0800

    latest

--- test/test_delete_policy_all_rate.py
+++ test/test_delete_policy_all_rate.py
@@ -4,6 +4,5 @@ import time
 ip = sys.argv[1]
 for i in range(10):
     for j in range(5):
-        print((i * 5 + j) * 1000)
         subprocess.run('python3 test_delete_policy.py '+ str(i * 2 + 3) + ' ' + ip + ':8008' +'' + str((i * 5 + j) * 1000), shell=True)
         time.sleep(30)
