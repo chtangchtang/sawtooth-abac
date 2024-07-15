0c8ce1cce65f54f1efc22d717afd9469c5b090b1
chtangchtang <1587758421@qq.com>
2024-07-15 17:57:16 +0800

    latest

--- test/test_check_inquiry_all_rate.py
+++ test/test_check_inquiry_all_rate.py
@@ -5,6 +5,5 @@ ip = sys.argv[1]
 for i in range(10):
     for j in range(5):
         print('Test rate: ', i, 'Times: ', j)
-        if subprocess.run('python3 test_check_inquiry.py '+ str(i * 2 + 3) + ' ' + ip + ':8008', shell=True).returncode:
-            raise RuntimeError('Test rate: ', i, 'Times: ', j, ' Failed')
+        subprocess.run('python3 test_check_inquiry.py '+ str(i * 2 + 3) + ' ' + ip + ':8008', shell=True)
         time.sleep(30)
--- test/test_delete_policy.py
+++ test/test_delete_policy.py
@@ -10,12 +10,14 @@ policy_range = range(int(sys.argv[3]), int(sys.argv[3]) + 1000)
 
 # write start epoch time
 client = InfluxDBClient(host='172.21.105.145', port='8086', username='admin', password='admin', database='metrics')
-points = []
-points.append({"measurement": "start_test_delete_policy", "fields": {'epoch_time': time.time()}})
-client.write_points(points)
+client.write_points([{"measurement": "start_test_delete_policy", "fields": {'epoch_time': time.time()}}])
+client.close()
 
 # start test
 for i in policy_range:
     command = "abac delete data/policy" + str(i) + ".json --url " + url + " &"
-    subprocess.run(command, shell=True)
+    if subprocess.run(command, shell=True).returncode:
+        client = InfluxDBClient(host='172.21.105.145', port='8086', username='admin', password='admin', database='metrics')
+        client.write_points([{"measurement": "error_test_delete_policy", "fields": {'epoch_time': time.time()}}])
+        client.close()
     time.sleep(1 / send_rate)
