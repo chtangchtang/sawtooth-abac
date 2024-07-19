1f1a9b45861cece4da61f8d0b4978bbf40bfff71
chtangchtang <1587758421@qq.com>
2024-07-19 16:15:30 +0800

    latest

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 /root/pbft_init.sh
 influx -username 'admin' -password 'admin' -execute 'drop database metrics'
 influx -username 'admin' -password 'admin' -execute 'create database metrics'
-nohub docker-compose -f /root/sawtooth-abac/test/pbft/5nodes.yaml up &
+nohup docker-compose -f /root/sawtooth-abac/test/pbft/5nodes.yaml up &
 sleep 30
 docker exec -it abac-client bash -c "cd test && abac add data/policy0.json --url rest-api--0:8008 && sleep 30 && python3 test_check_inquiry.py 10 rest-api-0:8008"
 sleep 30
