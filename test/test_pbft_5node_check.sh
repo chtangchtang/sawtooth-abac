dab03687aa7bc72fe17072927b054b48abb29002
chtangchtang <1587758421@qq.com>
2024-08-04 03:38:59 +0800

    latest

--- test/test_pbft_5node_check.sh
+++ test/test_pbft_5node_check.sh
@@ -7,19 +7,19 @@ do
         # Stop and remove all containers and volumes
         running_containers=$(docker ps -q)
         if [ -n "$running_containers" ]; then
-            docker stop $running_containers > /dev/null
+            docker stop $running_containers > /dev/null 2>&1
         fi
         all_containers=$(docker ps -a -q)
         if [ -n "$all_containers" ]; then
-            docker rm $all_containers > /dev/null
+            docker rm $all_containers > /dev/null 2>&1
         fi
         all_volumes=$(docker volume ls -q)
         if [ -n "$all_volumes" ]; then
-            docker volume rm $all_volumes > /dev/null
+            docker volume rm $all_volumes > /dev/null 2>&1
         fi
         all_networks=$(docker network ls -q)
         if [ -n "$all_networks" ]; then
-            docker network rm $all_networks > /dev/null
+            docker network rm $all_networks > /dev/null 2>&1
         fi
 
         # Remove and create InfluxDB database
@@ -27,7 +27,7 @@ do
         influx -username 'admin' -password 'admin' -execute 'create database metrics'
 
         # Start network
-        docker-compose -f /root/sawtooth-abac/test/pbft/5nodes.yaml up > /dev/null 2>&1
+        docker-compose -f /root/sawtooth-abac/test/pbft/5nodes.yaml up > /dev/null 2>&1 &
         # Wait for network to start
         sleep 30
 
@@ -38,14 +38,14 @@ do
         docker exec -it abac-client bash -c "cd test && python3 test_check_inquiry.py $rate rest-api-0:8008 && sleep 60"
 
         # Export InfluxDB database
-        influx_inspect export -datadir '/mnt/influxdb/data' -waldir '/mnt/influxdb/wal' -database metrics -out "/mnt/influxdb/output/pbft/5node/check_${rate}rate_${times}" > /dev/null
+        influx_inspect export -datadir '/mnt/influxdb/data' -waldir '/mnt/influxdb/wal' -database metrics -out "/mnt/influxdb/output/pbft/5node/check_${rate}rate_${times}" > /dev/null 2>&1
         # Analyse results
         python3 /root/sawtooth-abac/analysis/calculate_time.py /mnt/influxdb/output/pbft/5node/check_${rate}rate_${times} /root/pbft_5node_check_result.csv
 
         # Stop and remove all containers and volumes
-        docker stop $(docker ps -q) > /dev/null
-        docker rm $(docker ps -a -q) > /dev/null
-        docker volume rm $(docker volume ls -q) > /dev/null
-        docker network rm $(docker network ls -q) > /dev/null
+        docker stop $(docker ps -q) > /dev/null 2>&1
+        docker rm $(docker ps -a -q) > /dev/null 2>&1
+        docker volume rm $(docker volume ls -q) > /dev/null 2>&1
+        docker network rm $(docker network ls -q) > /dev/null 2>&1
     done
 done
