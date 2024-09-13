b982ea34b1a2ee0ba54196a3013a762c2b1012d1
chtangchtang <1587758421@qq.com>
2024-09-13 23:31:21 +0800

    latest

---
+++ test/test_raft_13node_check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+for rate in 1 3 5 7 9 11 13 15 17 19 21 
+do
+    for times in 0 1 2 3 4 5
+    do
+        ## Clean up network
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)
+        docker network rm $(docker network ls -q)
+        docker volume rm $(docker volume ls -q)
+        # Remove and create InfluxDB database
+        influx -username 'admin' -password 'admin' -execute 'drop database metrics'
+        influx -username 'admin' -password 'admin' -execute 'create database metrics'
+
+        # Up network
+        docker-compose -f /root/sawtooth-abac/test/raft/13nodes.yaml up &
+        # Wait for network to start
+        sleep 60
+        # Add a policy for testing check inquiry and test check inquiry
+        docker exec -it abac-client bash -c "cd test && abac add data/policy0.json --url rest-api-0:8008 && sleep 60 && python3 test_check_inquiry.py $rate rest-api-0:8008 && sleep 60"
+        # Export InfluxDB database
+        influx_inspect export -datadir '/mnt/influxdb/data' -waldir '/mnt/influxdb/wal' -database metrics -out "/mnt/influxdb/output/raft/13node/check_${rate}rate_${times}"
+        # Analyse results
+        python3 /root/sawtooth-abac/analysis/calculate_time_check.py /mnt/influxdb/output/raft/13node/check_${rate}rate_${times} /root/raft_13node_result.csv
+        # Down network
+        docker-compose -f /root/sawtooth-abac/test/raft/13nodes.yaml down -v
+    done
+done
+# Compress result files and remove them
+tar -czvf /mnt/influxdb/output/raft_13node_check.tar.gz /mnt/influxdb/output/raft/13node/
+rm /mnt/influxdb/output/raft/13node/*
---
+++ test/test_raft_9node_add+delete.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+for rate in 1 3 5 7 9 11 13 15 17 19 21 
+do
+    for times in 0 1 2 3 4 5
+    do
+        ## Clean up network
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)
+        docker network rm $(docker network ls -q)
+        docker volume rm $(docker volume ls -q)
+        # Remove and create InfluxDB database
+        influx -username 'admin' -password 'admin' -execute 'drop database metrics'
+        influx -username 'admin' -password 'admin' -execute 'create database metrics'
+
+        # Up network
+        docker-compose -f /root/sawtooth-abac/test/raft/9nodes.yaml up &
+        # Wait for network to start
+        sleep 60
+        # Test add and delete policy
+        docker exec -it abac-client bash -c "cd test && python3 test_add_policy.py $rate rest-api-0:8008 && sleep 60 && python3 test_delete_policy.py $rate rest-api-0:8008 && sleep 60"
+        # Export InfluxDB database
+        influx_inspect export -datadir '/mnt/influxdb/data' -waldir '/mnt/influxdb/wal' -database metrics -out "/mnt/influxdb/output/raft/9node/add+delete_${rate}rate_${times}"
+        # Analyse results
+        python3 /root/sawtooth-abac/analysis/calculate_time_add+delete.py /mnt/influxdb/output/raft/9node/add+delete_${rate}rate_${times} /root/raft_9node_result.csv
+        # Down network
+        docker-compose -f /root/sawtooth-abac/test/raft/9nodes.yaml down -v
+    done
+done
+# Compress result files and remove them
+tar -czvf /mnt/influxdb/output/raft_9node_add+delete.tar.gz /mnt/influxdb/output/raft/9node/
+rm /mnt/influxdb/output/raft/9node/*
